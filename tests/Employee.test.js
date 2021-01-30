a83992ca0c8ad1e7421de3769d501c2009be125e
Lachlan Cameron <lachlan.cameron@geckogrp.com.au>
2021-01-31 09:33:33 +1100

    GoogleCommmit

---
+++ tests/Employee.test.js
@@ -0,0 +1,14 @@
+//const index = require("../index");
+//const clsObj = require("../objClass");
+
+
+//test("Employee class", () => {
+//  expect('team').not.toMatch(/`1234567890!#$%&'()*+,-./:;<=>?@[]^_`{|}~/)`;
+//});
+
+//test('but there is a "stop" in Christoph', () => {
+//  expect('Christoph').toMatch(/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz/);/
+//});
+
+
+
